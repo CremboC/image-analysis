6b73db61816190f6c751d817b9f089dc63d65f28
CremboC <cremboc@gmail.com>
2015-01-07 13:58:49 +0000

    Centraliser - added licences

--- Centraliser/src/uk/ac/sanger/mig/xray/centraliser/CentraliserNodeView.java
+++ Centraliser/src/uk/ac/sanger/mig/xray/centraliser/CentraliserNodeView.java
@@ -1,61 +1,66 @@
+/*
+ * Copyright (c) 2014-2015 Genome Research Ltd.
+ * 
+ * Author: Mouse Informatics Group <team110g@sanger.ac.uk>
+ * This file is part of Centraliser.
+ * 
+ * Centraliser is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option ) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.ac.sanger.mig.xray.centraliser;
 
 import org.knime.core.node.NodeView;
 
 /**
- * <code>NodeView</code> for the "Centraliser" Node.
- * Centres a blob of white pixels to the centre of the image.
- *
+ * <code>NodeView</code> for the "Centraliser" Node. Centres a blob of white
+ * pixels to the centre of the image.
+ * 
  * @author Wellcome Trust Sanger Institute
  */
 public class CentraliserNodeView extends NodeView<CentraliserNodeModel> {
 
-    /**
-     * Creates a new view.
-     * 
-     * @param nodeModel The model (class: {@link CentraliserNodeModel})
-     */
-    protected CentraliserNodeView(final CentraliserNodeModel nodeModel) {
-        super(nodeModel);
+	/**
+	 * Creates a new view.
+	 * 
+	 * @param nodeModel
+	 *            The model (class: {@link CentraliserNodeModel})
+	 */
+	protected CentraliserNodeView(final CentraliserNodeModel nodeModel) {
+		super(nodeModel);
+	}
 
-        // TODO instantiate the components of the view here.
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void modelChanged() {
+		CentraliserNodeModel nodeModel = (CentraliserNodeModel) getNodeModel();
+		assert nodeModel != null;
+	}
 
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onClose() {
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void modelChanged() {
-
-        // TODO retrieve the new model from your nodemodel and 
-        // update the view.
-        CentraliserNodeModel nodeModel = 
-            (CentraliserNodeModel)getNodeModel();
-        assert nodeModel != null;
-        
-        // be aware of a possibly not executed nodeModel! The data you retrieve
-        // from your nodemodel could be null, emtpy, or invalid in any kind.
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void onClose() {
-    
-        // TODO things to do when closing the view
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void onOpen() {
-
-        // TODO things to do when opening the view
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onOpen() {
+	}
 
 }
-

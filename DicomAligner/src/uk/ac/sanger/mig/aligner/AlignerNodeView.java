04ca191f6ff5fc9afa273374ad356b95882f785c
CremboC <cremboc@gmail.com>
2015-01-07 13:59:35 +0000

    Aligner - added licences

--- DicomAligner/src/uk/ac/sanger/mig/aligner/AlignerNodeView.java
+++ DicomAligner/src/uk/ac/sanger/mig/aligner/AlignerNodeView.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2014-2015 Genome Research Ltd.
+ * 
+ * Author: Mouse Informatics Group <team110g@sanger.ac.uk>
+ * This file is part of Aligner.
+ * 
+ * Aligner is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option ) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.ac.sanger.mig.aligner;
 
 import org.knime.core.node.NodeView;
@@ -5,57 +25,42 @@ import org.knime.core.node.NodeView;
 /**
  * <code>NodeView</code> for the "Aligner" Node.
  * 
- *
+ * 
  * @author pi1
  */
 public class AlignerNodeView extends NodeView<AlignerNodeModel> {
 
-    /**
-     * Creates a new view.
-     * 
-     * @param nodeModel The model (class: {@link AlignerNodeModel})
-     */
-    protected AlignerNodeView(final AlignerNodeModel nodeModel) {
-        super(nodeModel);
+	/**
+	 * Creates a new view.
+	 * 
+	 * @param nodeModel
+	 *            The model (class: {@link AlignerNodeModel})
+	 */
+	protected AlignerNodeView(final AlignerNodeModel nodeModel) {
+		super(nodeModel);
+	}
 
-        // TODO instantiate the components of the view here.
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void modelChanged() {
+		AlignerNodeModel nodeModel = (AlignerNodeModel) getNodeModel();
+		assert nodeModel != null;
+	}
 
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onClose() {
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void modelChanged() {
-
-        // TODO retrieve the new model from your nodemodel and 
-        // update the view.
-        AlignerNodeModel nodeModel = 
-            (AlignerNodeModel)getNodeModel();
-        assert nodeModel != null;
-        
-        // be aware of a possibly not executed nodeModel! The data you retrieve
-        // from your nodemodel could be null, emtpy, or invalid in any kind.
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void onClose() {
-    
-        // TODO things to do when closing the view
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void onOpen() {
-
-        // TODO things to do when opening the view
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onOpen() {
+	}
 
 }
-

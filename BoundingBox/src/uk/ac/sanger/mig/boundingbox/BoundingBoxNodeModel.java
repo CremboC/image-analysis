8ede09e4b9e8e3b441014e383d28f2202d8839dd
CremboC <cremboc@gmail.com>
2015-01-07 13:57:50 +0000

    Bounding Box - added licences

--- BoundingBox/src/uk/ac/sanger/mig/boundingbox/BoundingBoxNodeModel.java
+++ BoundingBox/src/uk/ac/sanger/mig/boundingbox/BoundingBoxNodeModel.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2014-2015 Genome Research Ltd.
+ * 
+ * Author: Mouse Informatics Group <team110g@sanger.ac.uk>
+ * This file is part of Bounding Box.
+ * 
+ * Bounding Box is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option ) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.ac.sanger.mig.boundingbox;
 
 import java.util.HashMap;
@@ -32,8 +52,6 @@ import uk.ac.sanger.mig.boundingbox.utils.BoundingBox;
  * according to a threshold of pixels in a row/column
  * 
  * @author Wellcome Trust Sanger Institute
- * @author Paulius pi1@sanger.ac.uk
- * @author MIG Team team110dev@sanger.ac.uk
  * 
  * @param <T>
  */
--- BoundingBox/src/uk/ac/sanger/mig/boundingbox/utils/BoundingBox.java
+++ BoundingBox/src/uk/ac/sanger/mig/boundingbox/utils/BoundingBox.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2014-2015 Genome Research Ltd.
+ * 
+ * Author: Mouse Informatics Group <team110g@sanger.ac.uk>
+ * This file is part of Bounding Box.
+ * 
+ * Bounding Box is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option ) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.ac.sanger.mig.boundingbox.utils;
 
 import net.imglib2.RandomAccess;
@@ -10,9 +30,7 @@ import uk.ac.sanger.mig.analysis.nodetools.enums.ReturnType;
  * Wrapper class for the main logic for finding and calculating the Bounding
  * Box.
  * 
- * @author Paulius pi1@sanger.ac.uk
- * @author MIG Team team110dev@sanger.ac.uk
- * 
+ * @author pi1 pi1@sanger.ac.uk
  */
 public class BoundingBox<T extends RealType<T>> {
 
